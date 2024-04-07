780f09614e631c9e6774568635e493a9b7cd1b88
Rafael_Cabral <rafaelccmenezes@gmail.com>
2024-04-07 15:52:57 -0300

    Finalizacao da parte inicial, banco mysql integrado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
+const mysql = require('mysql2/promise');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
-
-module.exports = router
+ mysql.createConnection({host: 'localhost',user: 'root',password: '',
+ database: 'backend-I-express-mysql-RafaelCabral',port: 3306
+ }).then((connection) => {connection.query('SELECT * FROM clientes;')
+ .then((result) => {res.send(result[0]);});
+ });
+});
+module.exports = router;
