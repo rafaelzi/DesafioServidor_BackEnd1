dcb2e041da6946cef2a3ebcb045f99ab1af2c1c7
Rafael_Cabral <rafaelccmenezes@gmail.com>
2024-05-28 11:39:23 -0300

    Versão para apresentação DesafioBackEnd2

--- app.js
+++ app.js
@@ -17,34 +17,6 @@ var dotenv = require('dotenv');
 dotenv.config();
 console.log(process.env.MYSQL_BD);
 
-/*
-// Importar o pacote node-cache
-const NodeCache = require('node-cache');
-
-// Criar uma nova instância de cache
-const cache = new NodeCache();
-
-// Adicionar um item ao cache com uma chave e um valor
-cache.set('chave', 'valor');
-
-// Obter o valor do cache com a chave
-const valor = cache.get('chave');
-console.log('Valor do cache:', valor);
-
-// 60 segundos de tempo de vida
-cache.set('chave2', 'valor2', 60); 
-
-if (cache.has('chave2')) {
-    console.log('Chave2 está no cache:',
-cache.get('chave2')); // Deve imprimir 'valor2'
-} else {
-    console.log('Chave2 não está no cache');
-}
- 
-cache.del('chave');
-
-cache.flushAll();
-*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
